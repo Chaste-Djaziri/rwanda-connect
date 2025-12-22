88fe5774abd3f562700407e87b6f0a80cb217839
Chaste Djaziri <habimanahirwa@gmail.com>
2025-12-22 08:33:44 +0200

    Avoid cropping two-up images

--- src/components/feed/PostCard.tsx
+++ src/components/feed/PostCard.tsx
@@ -199,7 +199,7 @@ function ImageGrid({ images }: { images: any[] }) {
                 src={isGifUrl(image.fullsize) ? image.fullsize : image.thumb || image.fullsize}
                 alt={image.alt || 'Post image'}
                 className={`${isSingle ? 'w-auto max-w-full' : 'w-full'} ${
-                  isTwo ? 'object-cover' : 'object-contain'
+                  isTwo ? 'object-contain' : 'object-contain'
                 } bg-black ${
                   isSingle
                     ? 'h-auto'
