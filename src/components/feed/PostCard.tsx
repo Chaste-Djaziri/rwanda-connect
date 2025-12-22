1cb0e3975d5023841e46cd02dd0c4c353126e014
Chaste Djaziri <habimanahirwa@gmail.com>
2025-12-22 08:49:37 +0200

    Fill multi-image containers with cover

--- src/components/feed/PostCard.tsx
+++ src/components/feed/PostCard.tsx
@@ -201,7 +201,7 @@ function ImageGrid({ images }: { images: any[] }) {
                 src={isGifUrl(image.fullsize) ? image.fullsize : image.thumb || image.fullsize}
                 alt={image.alt || 'Post image'}
                 className={`${isSingle ? 'w-auto max-w-full' : 'w-full'} ${
-                  isTwo ? 'object-fill' : 'object-contain'
+                  isSingle ? 'object-contain' : 'object-cover'
                 } bg-black ${
                   isSingle
                     ? 'h-auto'
